6d04d8b5f29054ffdb7f6cee38ee51554eea98e0
Andrew Skelton <andrew.skelton@ucb.com>
2025-09-28 20:24:05 +0100

    config update

--- dvc_init.sh
+++ dvc_init.sh
@@ -1,20 +1,38 @@
+## Config for a pull
+pip install dvc
+pip install dvc-s3
+aws configure --profile dvc
+# Note, keys and secrets needed for s3 here
+git clone https://github.com/AndrewSkelton/GSE121212-Analysis-DVC 
+dvc pull
+##
+
+## Setting up the repo at first
+# Init
 git init
 dvc init
 git add .dvc .gitignore
 git commit -m "Initialize DVC"
 
+# Capture binary files for DVC
 dvc add *.RDS
 find ./Data ./Workstreams ./Pheno ./Reference -type f -name "*.RDS" | parallel -j1 dvc add {}
 dvc add *.gz
 find ./Data ./Workstreams ./Pheno ./Reference -type f -name "*.gz" | parallel -j1 dvc add {}
 
+# Commit to Git
 git add .
 git remote add origin https://github.com/AndrewSkelton/GSE121212-Analysis-DVC.git
 git commit -m "Initialise"
 git branch -M main
 git push -u origin main
 
+# Commit to dvcs
+dvc push 
+##
 
+
+## .gitignore
 # ## R environment management
 # Notebooks/renv/
 # !Notebooks/renv.lock
